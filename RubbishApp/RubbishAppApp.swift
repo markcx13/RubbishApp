0a3cadf66c78076b8a850530c9d9b9eb5a7d6d93
Mark Gould <M@rk.cx>
2022-03-13 19:29:10 +0000

    Initial Commit

---
+++ RubbishApp/RubbishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubbishAppApp.swift
+//  RubbishApp
+//
+//  Created by Mark Gould on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RubbishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
